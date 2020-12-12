db7ff6396e65af7a3c27db9c4656dfffd1382a86
HawkingLaugh <chanhugo@icloud.com>
2020-12-12 20:32:32 +0900

    added another number input

--- Sample_Code.py
+++ Sample_Code.py
@@ -1,11 +1,12 @@
 def numb_input():
     input1 = input("give me a number ")
     input2 = input("give me another number ")
-    return int(input1), int(input2)
+    input3 = input("third number please")
+    return int(input1), int(input2), int(input3)
 
 def addup():
-    num1, num2 = numb_input()
-    sum = num1 + num2
+    num1, num2, num3 = numb_input()
+    sum = num1 + num2 + num3
     print(sum)
 
 def main():
